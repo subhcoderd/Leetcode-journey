27131422e8f9fb582b832862af65f487050916cc
Subhadeep Das <subh6645@gmail.com>
2026-03-09 10:57:50 +0530

    Time: 78 ms (14.13%) | Memory: 94.3 MB (17.77%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -2,35 +2,29 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n = nums.size();
-        for(int i = 0; i <n;i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-            else nums[i]=1;
-        }
-        
-        int ps[n];
-        int count = 0;
-        unordered_map<int, int> m;
-        ps[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            ps[i] = ps[i - 1] + nums[i];
+
+        for(int i = 0; i < n; i++){
+            nums[i] = nums[i] % 2;
         }
 
-        for (int j = 0; j < n; j++) {
-            if (ps[j] == k) {
+        unordered_map<int,int> m;
+        int prefix = 0;
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            prefix += nums[i];
+
+            if(prefix == k){
                 count++;
             }
-            int val = ps[j] - k;
 
-            if (m.find(val) != m.end()) {
-                count += m[val];
+            if(m.find(prefix - k) != m.end()){
+                count += m[prefix - k];
             }
-            if (m.find(ps[j]) == m.end()) {
-                m[ps[j]] = 0;
-            }
-            m[ps[j]]++;
+
+            m[prefix]++;
         }
+
         return count;
     }
 };
